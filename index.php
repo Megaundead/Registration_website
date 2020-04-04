a8c3df46b2c69a68fa6af1d6deef5b73a0c21902
Fedor Kostrov <f.kostrov@8bitgroup.com>
2020-04-04 16:08:39 +0300

    initial front

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<title>Registration here</title>
+<head>
+    <title>Registration here</title>
+    <link href="./front/index.css" type="text/css" rel="stylesheet">
+</head>
 <body>
 <h1>Welcome to registration site</h1>
-<p>If you already registered click <a href="/back/login.php">here</a></p>
-<br>
-<h2>Fill your registration form here</h2>
-<form action="/back/registration.php" method="post">
-    <label>
-        <input type="text" name="login">
-    </label><br>
-    <label>
-        <input type="text" name="pass">
-    </label><br>
-    <input type="submit">
-</form>
+<div class="container">
+    <form action="/back/registration.php" method="post" class="form">
+        <h2>Fill your registration form here</h2>
+        <label>
+            <span class="label">Login</span>
+            <input type="text" name="login">
+        </label>
+        <label>
+            <span class="label">Password</span>
+            <input type="text" name="pass">
+        </label>
+        <button class="submit">
+            Submit
+        </button>
+    </form>
+    <p class="login">
+        Already have account? <a href="/back/login.php">Login!</a>
+    </p>
+</div>
 </body>
 </html>
 
